da211844adea28b83c2330cb89a86dc3957a75de
Antimka <antimkin@gmail.com>
2022-02-26 22:57:56 +0200

    commit

--- src/main/java/ru/levelp/at/training/Application.java
+++ src/main/java/ru/levelp/at/training/Application.java
@@ -7,7 +7,8 @@ public class Application {
         System.out.println("sum 2 + 2 = " + calculator.sum(2,2));
         System.out.println("==============");
         System.out.println(String.format("substract 4 - 5 = %d", calculator.substract(4, 5)));
-
+        System.out.println("==============");
+        System.out.println(String.format("2 in 7 power is %d", calculator.power(2, 7)));
     }
 
 }
--- src/main/java/ru/levelp/at/training/Calculator.java
+++ src/main/java/ru/levelp/at/training/Calculator.java
@@ -2,11 +2,31 @@ package ru.levelp.at.training;
 
 public class Calculator {
 
-    public int sum(int a, int b){
+    public int sum(int a, int b) {
         return a + b;
     }
 
-    public int substract(int a, int b){
+    public int substract(int a, int b) {
         return a - b;
+    }
+
+    public int divie(int a, int b) {
+        if (b == 0){
+            throw new IllegalArgumentException("b cannot be 0");
+        }
+        return a / b;
+    }
+
+    public int power(int a, int b){
+        if (b < 0){
+            throw new IllegalArgumentException("b cannot be negative");
+        }
+
+        int result = 1;
+
+        for (int i = 0; i < b; i++){
+            result *= a;
+        }
+        return result;
     }
 }
